8027b47890a4a7673ea2167f7c2bcea3f381a3ad
Osama Abdul Rahim <118612295+osamaabdo-dev@users.noreply.github.com>
2023-01-15 12:09:29 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+
+add_action('wp_head', function (){ // Any Code In Head Tag Here
+	?>
+	<script>
+
+	</script>
+	<?php
+});
+
+
+add_action('wp_footer', function (){ // Any Code In Footer Tag Here
+	?>
+	<script>
+
+	</script>
+	<?php
+});
+
+
+add_action('wp_body_open', function (){ // Any Code After Open "<body> Tag Here
+	?>
+	<script>
+
+	</script>
+	<?php
+});
